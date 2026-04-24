109b30ae6d7b3abb9e037c43c7ae8c4fed9b5019
Vladislav Medvedev <vmedvedevpro@gmail.com>
2026-04-24 14:53:37 +0300

    Added projects and initial setup

---
+++ src/OrderTrackingApp.Backend.WebApi/Program.cs
@@ -0,0 +1,16 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger()
+       .UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
